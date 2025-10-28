6efb621a8fc82025ed280a322535adbde8cf334f
សុខ ចិន្ដា <sokda106@gmail.com>
2025-10-28 06:25:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2720478864-92ec8",
+  "appId": "1:461351165997:web:671fb8de738755647418a4",
+  "apiKey": "AIzaSyDnEQip9-gORpUIh1jUV8TPFQ51jMeynOs",
+  "authDomain": "studio-2720478864-92ec8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "461351165997"
+};
